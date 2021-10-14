7138152651dbfaf41979470015c35b57b6c038ef
gabrielevensen <gabrielevensen96@gmail.com>
2021-10-14 15:19:08 +0200

    import time

--- celery_app.py
+++ celery_app.py
@@ -4,49 +4,39 @@ import json
 import os
 import timeit
 import re
-import time
+
 
 def make_celery(app):
     celery = Celery(app.import_name, backend='rpc://',
                     broker='pyamqp://guest@localhost//')
     celery.conf.update(app.config)
     TaskBase = celery.Task
+
     class ContextTask(TaskBase):
         abstract = True
+
         def __call__(self, *args, **kwargs):
             with app.app_context():
                 return TaskBase.__call__(self, *args, **kwargs)
+
     celery.Task = ContextTask
     return celery
+
 
 app = Flask(__name__)
 celery = make_celery(app)
 
-# -------- Flask Test -------- #
-# Returns entered name
-@app.route('/')
-def process1():
-    return 'OK request!'
-
-
-# -------- *1* Run celery task in Flask -------- #
-@app.route('/test/<name>')
-def proc(name):
-    return name
-
 
 # -------- *1* Run Tweet Counter in Flask -------- #
-@app.route('/task1')
+@app.route('/count_tweets')
 def process():
     result = count_pronouns.delay()
-    # time.sleep(10)
     return result.get()
 
-# -------- *1* Present result in Flask -------- #
-@celery.task(name='make_celery.count_pronouns')
-def count_pronouns():
-    start = timeit.default_timer()
 
+# -------- *1* Present result in Flask -------- #
+@celery.task()
+def count_pronouns():
     def find_word(w):
         return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
 
@@ -59,6 +49,7 @@ def count_pronouns():
     hen_counter = 0
     unique_counter = 0
 
+    start = timeit.default_timer()
     for subdir, dirs, files in os.walk('data'):
         for file in files:
             filepath = subdir + os.sep + file
@@ -85,15 +76,7 @@ def count_pronouns():
 
     file.close()
 
-    # print('Han appearing', han_counter, 'times.')
-    # print('Hon appearing', hon_counter, 'times.')
-    # print('Den appearing', den_counter, 'times.')
-    # print('Det appearing', det_counter, 'times.')
-    # print('Denna appearing', denna_counter, 'times.')
-    # print('Denne appearing', denne_counter, 'times.')
-    # print('Hen appearing', hen_counter, 'times.')
-    # print('Number of unique tweets:', unique_counter, '.')
-    #
+    # <--- Used to plot with flask: --->
     # objects = ('Han', 'Hon', 'Den', 'Det', 'Denna', 'Denne', 'Hen')
     # y_pos = np.arange(len(objects))
     # performance = [han_counter / unique_counter, hon_counter / unique_counter,
@@ -110,23 +93,17 @@ def count_pronouns():
     # plt.show()
 
     stop = timeit.default_timer()
+
+    # Print time taken to count tweets
     timer = stop - start
 
-    #print('Time: ', stop - start)
-
-    pronouns = ['han', 'hon', 'den', 'det', 'denna', 'denne', 'hen', 'Total unique tweets', 'Time for finding pronouns in seconds']
+    pronouns = ['han', 'hon', 'den', 'det', 'denna', 'denne', 'hen', 'Total unique tweets',
+                'Time for finding pronouns in seconds']
     counter = [han_counter, hon_counter, den_counter, det_counter, denna_counter, denne_counter, hen_counter,
                unique_counter, timer]
     result = dict(zip(pronouns, counter))
 
     return result
-
-    # lift = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-    # drag = ['high', 'low', 'medium', 'small', 'high', 'low', 'medium', 'high', 'small', 'small']
-    # result = dict(zip(lift, drag))
-    # time.sleep(10)
-    # return result
-
 
 
 if __name__ == '__main__':
