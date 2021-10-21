1a900ec75c85e905d46c4d41a0cafae8e185b230
gabrielevensen <64788960+gabrielevensen@users.noreply.github.com>
2021-10-21 02:43:13 +0200

    Update deploy-vm.sh

--- Initialize/deploy-vm.sh
+++ Initialize/deploy-vm.sh
@@ -1,19 +1,2 @@
 source UPPMAX\ 2021_1-5-openrc-3.sh
 python ssc-instance-userdata.py
-echo ""
-echo "<----- now wait for vm to contexualize, around 10m ----->"
-echo ""
-echo "Meanwhile look at the following output in order to use the application just deployed "
-echo ""
-openstack server show C3_task1 -f json
-echo ""
-echo "Description of above output:"
-echo ""		
-echo "UPPMAX 2021/1-5 Internal IPv4 Network: [
-      <Private IP>,
-      <Floating IP>  <-----------   This is the floating IP"
-echo ""
-echo "Run the followng command in the terminal with floating IP presented above: $ curl -i http://<Floating IP>:5000/start_count"
-echo ""
-echo "Or enter the following in your web browser: http://<Floating IP>:5000/start_count"
-
